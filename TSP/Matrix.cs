c934ff3a71f3fcc169a3092c6e35dc16ffc880d9
Fyodor-13-PM <mail1@mail.ru>
2015-10-25 23:05:35 +0400

    Написал класс Matrix, в нем: конструктор, инициализирующий матрицу нод и структуру нод

--- TSP/Matrix.cs
+++ TSP/Matrix.cs
@@ -7,6 +7,54 @@ namespace TSP
 {
     partial class Matrix
     {
-        //your code here
+        // Структура - элемент класса Matrix
+        public struct Node
+        {
+            public int i;// индекс i
+            public int j;// индекс j
+            public int value;// значение элемента массива
+        }
+
+        Node[,] matrix;// Массив Node
+
+        int size;
+
+        // Конструктор, принимающий двумерный массив int
+        public Matrix(int[,] source_matrix)
+        {
+            // Определяю размер массива
+            size = (int)Math.Sqrt(source_matrix.Length);
+
+            // Создаю массив Node - matrix
+            matrix = new Node[size, size];
+
+            // Заполняю каждый элемент массива matrix
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j].i = i;// полю i ноды присваиваю текущий индекс
+                    matrix[i, j].j = j;// полю j ноды присваиваю текущий индекс
+                    matrix[i, j].value = source_matrix[i, j];// полю value ноды присваиваю текущее значение source_matrix[i,j]
+                }
+            }
+        }
+
+        // Перегрузка оператора индексации
+        public int this[int its_i, int its_j]
+        {
+            get
+            {
+                // Возвращаю инт конкретной ноды
+                return matrix[its_i, its_j].value;
+            }
+
+            set
+            {
+                // присваиваю новое значение ноды текущей ноде
+                matrix[its_i, its_j].value = value;
+            }
+        }
     }
 }
+
